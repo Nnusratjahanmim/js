f5ba9c96c925c9ab7f96d12d536816e94bea2555
Nusrat Jahan Mim <77601025+Nnusratjahanmim@users.noreply.github.com>
2021-04-03 12:00:12 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+function btn1(){
+  document.getElementById("btnhead").style.color="red";
+}
+(function () {
+('[data-toggle="tooltip"]').tooltip()
+})
+function btn3(){
+  document.getElementById("fbicon").style.display="none"
+}
+function tab11(){
+  document.getElementById("tab1").style.color="red";
+}
+function tab22(){
+  document.getElementById("tab223").style.color="pink";
+}
+function tab33(){
+  document.getElementById("tab333").style.color="blue";
+}
+function clickbtn(){
+  document.getElementById("clickme").style.display="block";
+}
+
+
+	var x,t,x;
+x=20;
+t= 20;
+x=20+20;
+document.getElementById('variable').innerHTML=x;
+
+var na= "name";
+var me=20;
+var na= 130;
+document.getElementById('variable2').innerHTML=na+me+na;
+
+var  name=20+30+"name";
+document.getElementById('variable3').innerHTML=name;
+var name='26',name="25",age=22;
+document.getElementById('variable4').innerHTML=name;
+
+function hello(){
+document.getElementById('variable').innerHTML="hi";
+document.getElementById('variable').innerHTML="name";
+}
